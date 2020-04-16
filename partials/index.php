70522f8356ccd31eea118b0f1d566d3535cf30b6
Pepef <pepef@localhost.localdomain>
2020-04-16 15:57:53 +0000

    XTCore

---
+++ partials/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Bienvenido</title>
+        <link rel="stylesheet" href="/assets/css/style.css">
+    </head>
+    <body>
+     <h1>Por favor inicia sesion o registrate</h1>
+     <a href="login.php">Iniciar Sesion</a> ó 
+     <a href="Signup.php">Crear cuenta</a>
+     
+    </body>
+</html>    
